40411673c5616162cdc2a336af3c5b6bebdac533
dorianrod <33071955+dorianrod@users.noreply.github.com>
2019-09-27 09:23:47 +0200

    Make it work on real navigator

--- src/setup.js
+++ src/setup.js
@@ -1,19 +1,34 @@
 import { LocalStorage } from './localstorage';
 
-if (typeof global._localStorage !== 'undefined') {
-  Object.defineProperty(global, '_localStorage', {
+
+try {
+
+  if (typeof global._localStorage !== 'undefined') {
+    Object.defineProperty(global, '_localStorage', {
+      value: new LocalStorage(jest),
+      writable: false
+    });
+  } else {
+    global.localStorage = new LocalStorage(jest);
+  }
+
+  if (typeof global._sessionStorage !== 'undefined') {
+    Object.defineProperty(global, '_sessionStorage', {
+      value: new LocalStorage(jest),
+      writable: false
+    });
+  } else {
+    global.sessionStorage = new LocalStorage(jest);
+  }
+
+} catch(err) {
+  Object.defineProperty(global, 'localStorage', {
     value: new LocalStorage(jest),
-    writable: false,
+    writable: false
   });
-} else {
-  global.localStorage = new LocalStorage(jest);
+  Object.defineProperty(global, 'sessionStorage', {
+    value: new LocalStorage(jest),
+    writable: false
+  });
 }
 
-if (typeof global._sessionStorage !== 'undefined') {
-  Object.defineProperty(global, '_sessionStorage', {
-    value: new LocalStorage(jest),
-    writable: false,
-  });
-} else {
-  global.sessionStorage = new LocalStorage(jest);
-}
